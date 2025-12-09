a4a8060d8e2c40dd8231e15eecd2363bbde9a673
mdarim <mdarim@gmail.com>
2025-12-10 03:22:33 +0400

    java 23 gradle 9 spring boot 4

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -15,7 +19,7 @@ public class DemoApplication {
     @Bean
     public CommandLineRunner springBootMain() throws Exception {
         return args -> {
-            System.out.println("*** Start Spring Boot Project ***");
+            log.info("*** welcome to project runner ***");
         };
     }
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,14 +1,16 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-		System.out.println("*** Start Spring Boot Test Project ***");
+        log.debug("*** welcome to test ***");
 	}
 
 }
